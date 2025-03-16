90a76d4dc2ac9ebfe72548a97892740476756d14
ztgd <1927552117@qq.com>
2025-03-16 21:22:07 +0800

    更新：炼金的信息补全

--- js/atelier.js
+++ js/atelier.js
@@ -19,20 +19,191 @@ document.addEventListener('DOMContentLoaded', function () {
             link: "https://www.gamecity.com.tw/atelier/marie-re/"
         },
         a1: {
-            title: "玛莉的炼金工房 ～萨尔布鲁克的炼金术士～ ",
+            title: "玛莉的炼金工房 ～萨尔布鲁克的炼金术士～",
             images: {
                 bottom: "../resources/a1/2.jpg",
                 middle: "../resources/a1/1.png",
                 top: "../resources/a1/3.png"
             },
-            link: ""
+            link: "http://www.gust.co.jp/products/A3/history/marie.html"
+        },
+        a2: {
+            title: "艾莉的炼金工房 ～萨尔布鲁克的炼金术士2～",
+            images: {
+                bottom: "../resources/a2/2.jpg",
+                middle: "../resources/a2/1.png",
+                top: "../resources/a2/3.png"
+            },
+            link: "http://www.gust.co.jp/products/A3/history/elie.html"
+        },
+        a3: {
+            title: "莉莉的炼金工房 ～萨尔布鲁克的炼金术士3～",
+            images: {
+                bottom: "../resources/a3/2.jpg",
+                middle: "../resources/a3/1.png",
+                top: "../resources/a3/3.png"
+            },
+            link: "http://www.gust.co.jp/products/A3/newgame/top.html"
+        },
+        a4: {
+            title: "a2-a10",
+            images: {
+                bottom: "../resources/a4/2.jpg",
+                middle: "../resources/a4/1.png",
+                top: "../resources/a4/3.png"
+            },
+            link: "http://www.gust.co.jp/products/a4/index.html"
+        },
+        a11: {
+            title: "萝乐娜的炼金工房 ～亚兰德之炼金术士～",
+            images: {
+                bottom: "../resources/a11/2.jpg",
+                middle: "../resources/a11/1.png",
+                top: "../resources/a11/3.png"
+            },
+            link: "https://www.gamecity.ne.jp/atelier/arlanddx/index.html"
+        },
+        a12: {
+            title: "托托莉的炼金工房 ～亚兰德之炼金术士2～",
+            images: {
+                bottom: "../resources/a12/2.jpg",
+                middle: "../resources/a12/1.png",
+                top: "../resources/a12/3.png"
+            },
+            link: "https://www.gamecity.ne.jp/atelier/arlanddx/index.html"
+        },
+        a13: {
+            title: "梅露露的炼金工房 ～亚兰德之炼金术士3～",
+            images: {
+                bottom: "../resources/a13/2.jpg",
+                middle: "../resources/a13/1.png",
+                top: "../resources/a13/3.png"
+            },
+            link: "https://www.gamecity.ne.jp/atelier/arlanddx/index.html"
+        },
+        a14: {
+            title: "爱夏的炼金工房 ～黄昏大地之炼金术士～",
+            images: {
+                bottom: "../resources/a14/2.jpg",
+                middle: "../resources/a14/1.png",
+                top: "../resources/a14/3.png"
+            },
+            link: "https://www.gamecity.com.tw/atelier/duskdx/"
+        },
+        a15: {
+            title: "爱丝卡&罗吉的炼金工房 ～黄昏天空之炼金术士～",
+            images: {
+                bottom: "../resources/a15/2.jpg",
+                middle: "../resources/a15/1.png",
+                top: "../resources/a15/3.png"
+            },
+            link: "https://www.gamecity.com.tw/atelier/duskdx/"
+        },
+        a16: {
+            title: "夏莉的炼金工房 ～黄昏海洋之炼金术士～",
+            images: {
+                bottom: "../resources/a16/2.jpg",
+                middle: "../resources/a16/1.png",
+                top: "../resources/a16/3.png"
+            },
+            link: "https://www.gamecity.com.tw/atelier/duskdx/"
+        },
+        a17: {
+            title: "苏菲的炼金工房 ～不可思议书的炼金术士～",
+            images: {
+                bottom: "../resources/a17/2.jpg",
+                middle: "../resources/a17/1.png",
+                top: "../resources/a17/3.png"
+            },
+            link: "https://www.gamecity.com.tw/atelier/mysteriousdx/"
+        },
+        a18: {
+            title: "菲利丝的炼金工房 ～不可思议旅的炼金术士～",
+            images: {
+                bottom: "../resources/a18/2.jpg",
+                middle: "../resources/a18/1.png",
+                top: "../resources/a18/3.png"
+            },
+            link: "https://www.gamecity.com.tw/atelier/mysteriousdx/"
+        },
+        a19: {
+            title: "莉迪&苏瑞的炼金工房 ～不可思议绘画的炼金术士～",
+            images: {
+                bottom: "../resources/a19/2.jpg",
+                middle: "../resources/a19/1.png",
+                top: "../resources/a19/3.png"
+            },
+            link: "https://www.gamecity.com.tw/atelier/mysteriousdx/"
+        },
+        a20: {
+            title: "露露亚的炼金工房 ～亚兰德之炼金术士4～",
+            images: {
+                bottom: "../resources/a20/2.jpg",
+                middle: "../resources/a20/1.png",
+                top: "../resources/a20/3.png"
+            },
+            link: "https://www.gamecity.com.tw/atelier/lulua/"
+        },
+        a21: {
+            title: "莱莎的炼金工房～常暗女王与秘密藏身处～",
+            images: {
+                bottom: "../resources/a21/2.jpg",
+                middle: "../resources/a21/1.png",
+                top: "../resources/a21/3.png"
+            },
+            link: "https://www.gamecity.com.tw/atelier/ryza/"
+        },
+        a22: {
+            title: "莱莎的炼金工房2 ～失落传说与秘密妖精～",
+            images: {
+                bottom: "../resources/a22/2.jpg",
+                middle: "../resources/a22/1.png",
+                top: "../resources/a22/3.png"
+            },
+            link: "https://www.gamecity.com.tw/atelier/ryza2/"
+        },
+        a23: {
+            title: "苏菲的炼金工房2 ～不可思议梦的炼金术士～",
+            images: {
+                bottom: "../resources/a23/2.jpg",
+                middle: "../resources/a23/1.png",
+                top: "../resources/a23/3.png"
+            },
+            link: "https://www.gamecity.com.tw/atelier/sophie2/"
+        },
+        a24: {
+            title: "莱莎的炼金工房3 ～终结之炼金术士与秘密钥匙～",
+            images: {
+                bottom: "../resources/a24/2.jpg",
+                middle: "../resources/a24/1.png",
+                top: "../resources/a24/3.png"
+            },
+            link: "https://www.gamecity.com.tw/atelier/ryza3/"
+        },
+        a25: {
+            title: "蕾斯莱莉娅娜的炼金工房 ～忘却的炼金术与极夜的解放者～",
+            images: {
+                bottom: "../resources/a25/2.jpg",
+                middle: "../resources/a25/1.png",
+                top: "../resources/a25/3.png"
+            },
+            link: "https://resleriana.atelier.games/sch/"
+        },
+        a25ii: {
+            title: "蕾斯莱莉娅娜的炼金工房～千之国与万物的管理者～",
+            images: {
+                bottom: "../resources/a25ii/2.jpg",
+                middle: "../resources/a25/1.png",
+                top: "../resources/a25ii/3.png"
+            },
+            link: "https://resleriana.atelier.games/sch/"
         },
         a25rw: {
             title: "红色的炼金术士和白色的守护者 ～蕾斯莱莉娅娜的炼金工房～",
             images: {
-                bottom: "../resources/a25w/2.jpg",
-                middle: "../resources/a25w/1.png",
-                top: "../resources/a25w/3.png"
+                bottom: "../resources/a25rw/2.jpg",
+                middle: "../resources/a25rw/1.png",
+                top: "../resources/a25rw/3.png"
             },
             link: "https://atelier.games/resleriana_rw/tch/"
         },
